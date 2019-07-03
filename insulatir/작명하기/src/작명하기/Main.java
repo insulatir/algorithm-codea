8a5eaf481bbfe5657fc063d8dbe8faf4739442ba
bluesocks <s42415466@gmail.com>
2019-07-03 11:04:25 +0900

    문제 초기 조건

---
+++ insulatir/작명하기/src/작명하기/Main.java
@@ -0,0 +1,13 @@
+package 작명하기;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String S = scan.nextLine() + scan.nextLine();
+		
+		scan.close();
+	}
+
+}
