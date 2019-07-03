bec55c034c53f0a28f0ef2379329cf9a312d8ee4
bluesocks <s42415466@gmail.com>
2019-07-03 12:42:11 +0900

    pi[i] = N[..i]의 접미사도 되고 접두사도 되는 문자열의 최대 길이

--- insulatir/재하의 금고/src/Main.java
+++ insulatir/재하의 금고/src/Main.java
@@ -15,4 +15,25 @@ public class Main {
 		}
 	}
 
+	public static int[] getPartialMatch(String N) {
+		int m = N.length();
+		int[] pi = new int[m];
+		int begin = 1;
+		int matched = 0;
+		while (begin + matched < m) {
+			if (N.charAt(begin + matched) == N.charAt(matched)) {
+				matched += 1;
+				pi[begin+matched-1] = matched;
+			} else {
+				if (matched == 0) {
+					begin += 1;
+				} else {
+					begin += matched - pi[matched-1];
+					matched = pi[matched-1];
+				}
+			}
+		}
+		
+		return pi;
+	}
 }
