0c54fedfa42f86f57bb599c6736362f4a247f8d0
bluesocks <s42415466@gmail.com>
2019-07-03 12:41:47 +0900

    문제 초기 조건

---
+++ insulatir/재하의 금고/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int C = scan.nextInt();
+		for (int i = 0; i < C; i++) {
+			int N = scan.nextInt();
+			int result = 0;
+			String ori = scan.next();
+			for (int j = 0; j < N; j++) {
+				String tar = scan.next();
+			}
+		}
+	}
+
+}
