75e4e7c611d6ef47ca57f55eb1087a966b9376d9
bluesocks <s42415466@gmail.com>
2019-07-06 11:47:44 +0900

    Buffered를 위한 throws

--- insulatir/삽입 정렬 뒤집기/src/Main.java
+++ insulatir/삽입 정렬 뒤집기/src/Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
 	static int n;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
