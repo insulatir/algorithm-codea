4a7e4d2be73e8ca2f12dcaeaa517837f80d8b942
bluesocks <s42415466@gmail.com>
2019-07-07 10:35:38 +0900

    maxHeap의 크기는 minHeap의 크기와 같거나 1 더 큼

--- insulatir/01655 가운데를 말해요/src/Main.java
+++ insulatir/01655 가운데를 말해요/src/Main.java
@@ -13,5 +13,13 @@ public class Main {
 	public static void runningMedian(int n) {
 		PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
 		PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+	
+		for (int cnt = 1; cnt <= n; cnt++) {
+			if (maxHeap.size() == minHeap.size()) {
+				maxHeap.add(scan.nextInt());
+			} else {
+				minHeap.add(scan.nextInt());
+			}
+		}
 	}
 }
