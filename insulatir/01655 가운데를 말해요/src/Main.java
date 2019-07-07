8557007ab1f20cc3212d087ba341681e1a50fbf1
bluesocks <s42415466@gmail.com>
2019-07-07 10:34:08 +0900

    문제 초기 조건

---
+++ insulatir/01655 가운데를 말해요/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+	static Scanner scan;
+
+	public static void main(String[] args) {
+		scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		
+		scan.close();
+	}
+
+}
